475fdaf3f4415d9cce80c4c29cd3cac56d2e0a95
Ivan Yurchenkov <qwerty29544@gmail.com>
2018-07-18 13:02:43 +0300

    Test for PQ was added

--- src/tests/PriorityQueues/PriorityQueueArrayClassTest.java
+++ src/tests/PriorityQueues/PriorityQueueArrayClassTest.java
@@ -37,35 +37,78 @@ public class PriorityQueueArrayClassTest {
 
     @Test
     public void insert() throws Exception {
+        assertEquals(this.priorityQueueArray.peekMin(),-21);
+        this.priorityQueueArray.insert(-22);
+        assertEquals(this.priorityQueueArray.peekMin(),-22);
+        this.priorityQueueArray.insert(10);
+        assertEquals(this.priorityQueueArray.getQueArray()[2],10);
+        assertEquals(this.priorityQueueArray.isFull(),true);
+    }
 
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void insertException(){
+        for (int i=123; i<=132; i++){
+            this.priorityQueueArray.insert(i);
+        }
     }
 
     @Test
     public void remove() throws Exception {
+        assertEquals(this.priorityQueueArray.getQueArray()[3],-21);
+        this.priorityQueueArray.remove();
+        assertEquals(this.priorityQueueArray.getQueArray()[2],9);
+        this.priorityQueueArray.remove();
+        this.priorityQueueArray.remove();
+        this.priorityQueueArray.remove();
+        assertEquals(this.priorityQueueArray.isEmpty(),true);
     }
 
     @Test
     public void peekMin() throws Exception {
+        assertEquals(this.priorityQueueArray.peekMin(),-21);
+        this.priorityQueueArray.insert(-1230);
+        this.priorityQueueArray.insert(0);
+        assertEquals(this.priorityQueueArray.peekMin(),-1230);
     }
 
     @Test
     public void isEmpty() throws Exception {
+        assertEquals(this.emptyQ.isEmpty(),true);
+        this.emptyQ.insert(2);
+        assertEquals(this.emptyQ.isEmpty(),false);
+        this.emptyQ.remove();
+        assertEquals(this.emptyQ.isEmpty(),true);
     }
 
     @Test
     public void isFull() throws Exception {
+        assertEquals(this.fullQ.isFull(),false);
+        this.fullQ.insert(3);
+        assertEquals(this.fullQ.isFull(),true);
     }
 
     @Test
     public void getMaxSize() throws Exception {
+        assertEquals(this.fullQ.getMaxSize(),3);
+        assertEquals(this.priorityQueueArray.getMaxSize(),6);
     }
 
     @Test
     public void getQueArray() throws Exception {
+        assertNotNull(this.priorityQueueArray.getQueArray());
+        assertEquals(this.priorityQueueArray.getQueArray()[0],42);
+        assertEquals(this.emptyQ.getQueArray()[0],0);
     }
 
     @Test
     public void getnItems() throws Exception {
+        assertEquals(this.priorityQueueArray.getnItems(),4);
+        this.priorityQueueArray.remove();
+        assertEquals(this.priorityQueueArray.getnItems(),3);
+        this.priorityQueueArray.remove();
+        this.priorityQueueArray.remove();
+        this.priorityQueueArray.remove();
+        assertEquals(this.priorityQueueArray.getnItems(),0);
     }
 
 }
